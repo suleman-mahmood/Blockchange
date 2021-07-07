6065d0b4ef45c76b318c6175d5b9820c2a1c7be8
Suleman Mahmood <sulemanmahmood99@gmail.com>
2021-07-07 22:51:39 +0500

    initial-commit

---
+++ blockchange/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <div className="row">
+          <div className="col s12">
+            <h6 className="center-align">Welcome to BlockChange!</h6>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
