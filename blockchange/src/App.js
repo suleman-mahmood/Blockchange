8611c544716a7dc7b6ca80266a0a242033d33b55
Suleman Mahmood <sulemanmahmood99@gmail.com>
2021-07-08 02:22:17 +0500

    firebase init

--- blockchange/src/App.js
+++ blockchange/src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import firebase from './firebase-init'
 
 function App() {
+
+  useEffect(() => {
+    // firebase.auth().createUserWithEmailAndPassword("sulemanmahmood@gmail.com", "12345678")
+    // .then((userCredential) => {
+    //   // Signed in 
+    //   var user = userCredential.user;
+    //   console.log(userCredential);
+    //   // ...
+    // })
+    // .catch((error) => {
+    //   var errorCode = error.code;
+    //   var errorMessage = error.message;
+    //   console.log(errorMessage);
+    //   // ..
+    // });
+  })
+
   return (
     <div className="container">
         <div className="row">
---
+++ blockchange/src/firebase-init.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyCC0SsEsaUgp39eKbVkCd2I4lhWkRgUhDo",
+  authDomain: "blockchange-29151.firebaseapp.com",
+  projectId: "blockchange-29151",
+  storageBucket: "blockchange-29151.appspot.com",
+  messagingSenderId: "512304975548",
+  appId: "1:512304975548:web:f09f18a466dd17193a0ab9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
